4970ef92fac92e40bf77e724eb9dbcd0777e5292
Pradeep <2320090007@klh.edu.in>
2026-05-07 10:14:17 +0530

    Added Sort an Array solution

---
+++ Arrays & Hashing/SortAnArray.java
@@ -0,0 +1,39 @@
+// Topic:- Arrays
+// Time Complexity:-O(n * log n)
+// Space Complexity:-O(n)
+
+class Solution {
+    int[] temp;
+    public void merge(int[] a, int lo, int mid, int hi){
+        int left = lo, right = mid + 1, idx = 0;
+        while(left <= mid && right <= hi){
+            if(a[left] <= a[right]){
+                temp[idx++] = a[left++];
+            }
+            else{
+                temp[idx++] = a[right++];
+            }
+        }
+        while(left <= mid){
+            temp[idx++] = a[left++];    
+        }
+        while(right <= hi){
+            temp[idx++] = a[right++];    
+        }
+        for(int i = lo; i <= hi; i++){
+            a[i] = temp[i - lo];
+        }
+    }
+    public void mergeSort(int[] a, int lo, int hi){
+        if(lo >= hi) return;
+        int mid = (lo + hi) / 2;
+        mergeSort(a, lo, mid);
+        mergeSort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+    public int[] sortArray(int[] nums) {
+        temp = new int[nums.length];
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+}
