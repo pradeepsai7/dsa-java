2a0d1b4b1bf97d803f6c94733536e2a1d2ab4bb7
Pradeep <2320090007@klh.edu.in>
2026-05-08 00:24:40 +0530

    Added Top K Frequent Elements solution

---
+++ Arrays & Hashing/TopKFrequentElement.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        List<Integer>[] lst = new List[nums.length + 1];
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for(int num : nums){
+            mp.put(num, mp.getOrDefault(num, 0) + 1);
+        }
+        for(int key : mp.keySet()){
+            int freq = mp.get(key);
+            if(lst[freq] == null){
+                lst[freq] = new ArrayList<>();
+            }
+            lst[freq].add(key);
+        }
+        int[] ans = new int[k];
+        int idx = 0;
+        for(int i = lst.length - 1; i >= 0 && idx < k; i--){
+            if(lst[i] != null){
+                for(int num : lst[i]){
+                    ans[idx++] = num;
+                    if(idx == k){
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
